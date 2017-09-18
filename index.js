ff65c377fd1d213a073d69d22507b917ac558a73
Bruno Assarisse <bruno@gatec.com.br>
2017-09-18 11:22:55 -0300

    Added request helper and related util

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 import lang from './lang'
 import EventStack from './EventStack'
+import request from './request'
 
 export {
     lang,
     EventStack,
+    request,
 }
---
+++ request/index.js
@@ -0,0 +1,61 @@
+
+import request from 'superagent'
+
+import makeAbortablePromise from '../util/makeAbortablePromise'
+
+function clearObject(obj) {
+
+    if (obj) {
+        Object.keys(obj).forEach(prop => {
+            if (typeof obj[prop] === 'undefined')
+                delete obj[prop]
+        })
+    }
+
+    return obj
+}
+
+function createRequest(url, method = 'get') {
+    var type = (method || '').toLowerCase()
+
+    if (type === 'get')
+        return request.get(url)
+
+    if (type === 'post')
+        return request.post(url)
+
+    if (type === 'put')
+        return request.put(url)
+
+    if (type === 'head')
+        return request.head(url)
+
+    return request.get(url)
+}
+
+export function fetch({ method, url, query, data, type, headers }) {
+
+    var req = createRequest(url, method)
+        .type(type)
+        .query(clearObject(query || {}))
+        .send(clearObject(data))
+
+    if (headers)
+        req = req.set(headers)
+
+    var abortablePromise = makeAbortablePromise(req.then())
+    var abort = abortablePromise.abort
+
+    abortablePromise.request = req
+
+    abortablePromise.abort = () => {
+        if (req && req.abort)
+            req.abort()
+        if (abort)
+            abort()
+    }
+
+    return abortablePromise
+}
+
+export default fetch
