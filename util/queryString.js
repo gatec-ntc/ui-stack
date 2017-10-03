45c8a61ae4f727cf2eeffd7012d153ce7b811d24
Bruno Assarisse <bruno@gatec.com.br>
2017-10-03 17:00:10 -0300

    Fixed queryString util

--- util/queryString.js
+++ util/queryString.js
@@ -3,11 +3,11 @@ import queryString from 'query-string'
 
 export default {
 
-    queryString(queryObject) {
+    stringify(queryObject) {
         return queryObject ? '?' + queryString.stringify(queryObject) : ''
     },
 
-    query(search) {
+    parse(search) {
         return search ? queryString.parse(search) : {}
     },
 
