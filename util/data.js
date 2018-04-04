5557c1875125426b4913f76170b06c1f270f877f
Bruno Assarisse <asteriscko@gmail.com>
2018-04-04 11:59:59 -0300

    Added deepClone util

--- util/data.js
+++ util/data.js
@@ -19,3 +19,24 @@ export function clearUndefined(obj) {
 export function isEqual(a, b, opt) {
     return equal(clearUndefined(a), clearUndefined(b), opt)
 }
+
+// copied from https://stackoverflow.com/a/40294058
+export function deepClone(obj, hash = new WeakMap()) {
+
+    if (Object(obj) !== obj) // primitives
+        return obj;
+    if (hash.has(obj)) // cyclic reference
+        return hash.get(obj);
+
+    const result = obj instanceof Date ? new Date(obj)
+                 : obj instanceof RegExp ? new RegExp(obj.source, obj.flags)
+                 : obj.constructor ? new obj.constructor() 
+                 : Object.create(null);
+
+    hash.set(obj, result);
+
+    if (obj instanceof Map)
+        Array.from(obj, ([key, val]) => result.set(key, deepClone(val, hash)));
+
+    return Object.assign(result, ...Object.keys(obj).map(key => ({ [key]: deepClone(obj[key], hash) })));
+}
