857b4088cd4370a4c0df245777ceed7610ab076c
Bruno Assarisse <bruno@assarisse.com.br>
2018-06-27 13:20:17 -0300

    Added Portal component

---
+++ ui/Portal.jsx
@@ -0,0 +1,56 @@
+
+import { Component } from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+const portalRoot = document.body
+
+export default class Portal extends Component {
+
+    static propTypes = {
+        className: PropTypes.string,
+        children: PropTypes.node,
+        onPortalDidMount: PropTypes.func,
+        onPortalWillUnmount: PropTypes.func,
+    }
+
+    constructor(props) {
+        super(props)
+
+        this._target = document.createElement('div')
+        this._target.className = this.props.className || ''
+
+    }
+
+    componentDidMount() {
+
+        portalRoot.appendChild(this._target)
+
+        if (typeof this.props.onPortalDidMount === 'function')
+            this.props.onPortalDidMount(this._target)
+
+    }
+
+    componentDidUpdate() {
+
+        if (this._target)
+            this._target.className = this.props.className || ''
+
+    }
+
+    componentWillUnmount() {
+
+        if (typeof this.props.onPortallWillUnmount === 'function')
+            this.props.onPortalWillUnmount(this._target)
+
+        portalRoot.removeChild(this._target)
+
+        this._target = null
+
+    }
+
+    render() {
+        return ReactDOM.createPortal(this.props.children, this._target)
+    }
+
+}
